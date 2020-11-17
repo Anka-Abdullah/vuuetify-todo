a85381d0e7c9c077bf036ea8f68cbfab78d3268f
Anka-Abdullah <ankaabdullah.id@gmail.com>
2020-11-17 10:36:06 +0700

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, odit hic. Earum provident repudiandae ex consequatur doloremque magni at dicta recusandae maiores officiis adipisci, error dolorem laudantium deserunt sit culpa omnis porro labore hic veritatis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, odit hic. Earum provident repudiandae ex consequatur doloremque magni at dicta recusandae maiores officiis adipisci, error dolorem laudantium deserunt sit culpa omnis porro labore hic veritatis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, odit hic. Earum provident repudiandae ex consequatur doloremque magni at dicta recusandae maiores officiis adipisci, error dolorem laudantium deserunt sit culpa omnis porro labore hic veritatis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, odit hic. Earum provident repudiandae ex consequatur doloremque magni at dicta recusandae maiores officiis adipisci, error dolorem laudantium deserunt sit culpa omnis porro labore hic veritatis.</p>
   </div>
 </template>
